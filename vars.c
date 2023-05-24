ad0d36a44794ccf548cd3a8b3f4191381bb367b1
Angosepeter <kevinangose619@gmail.com>
2023-05-24 01:54:02 -0700

    adding email

--- vars.c
+++ vars.c
@@ -133,7 +133,7 @@ int replace_vars(info_t *info)
 					_strdup(_strchr(node->str, '=') + 1));
 			continue;
 		}
-		replace_string(&info->argv[i], _strdup(""));
+		replace_string(&info->argv[i],_strdup(""));
 
 	}
 	return (0);
