276960ddedcf7875e151111ffd6cc79323151633
Nasibodula <nasiochudo@gmail.com>
2023-05-25 23:31:28 +0300

    latest updates

--- getenvrnmt.c
+++ getenvrnmt.c
@@ -7,7 +7,7 @@
 */
 char **get_environ(info_t *info)
 {
-if (!(info -> environ) || (info -> env_changed))
+if (!info -> environ || info -> env_changed)
 {
 info->environ = list_to_strings(info -> env);
 info->env_changed = 0;
